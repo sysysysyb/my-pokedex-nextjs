07b57156edd0bb2f9c48dab403a9432c1b3ee2d7
seo_0 <87892147+sysysysyb@users.noreply.github.com>
2025-06-17 00:14:24 +0900

    Update and rename Loading.jsx to loading.jsx

--- src/app/search/loading.jsx
+++ src/app/search/loading.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import RotateGif from "@/images/rotate.gif";
 
-export default function Loading() {
+export default function loading() {
   return (
     <div className="w-full h-full flex justify-center items-center">
       <Image src={RotateGif} alt="loading" className="size-20" />
